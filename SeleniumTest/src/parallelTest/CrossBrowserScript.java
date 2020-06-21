c48915d727e492c5722fcd4c5ddb249aa5da6315
Mihaela Chirila <58293543+MihaelaChirila@users.noreply.github.com>
2020-06-22 02:40:07 +0300

    Update CrossBrowserScript.java

--- SeleniumTest/src/parallelTest/CrossBrowserScript.java
+++ SeleniumTest/src/parallelTest/CrossBrowserScript.java
@@ -14,37 +14,22 @@ public class CrossBrowserScript {
 
     WebDriver driver;
 
-    /**
-     * This function will execute before each Test tag in testng.xml
-     *
-     * @param browser
-     * @throws Exception
-     */
     @Parameters("browser")
     @BeforeTest
 
     public void setup(String browser) throws Exception {
-        //Check if parameter passed from TestNG is 'firefox'
         if (browser.equalsIgnoreCase("firefox")) {
-            //create firefox instance
             System.setProperty("webdriver.gecko.driver", "C:\\SeleniumDrivers\\geckodriver.exe");
             driver = new FirefoxDriver();
         }
-        //Check if parameter passed as 'chrome'
         else if (browser.equalsIgnoreCase("chrome")) {
-            //set path to chromedriver.exe
             System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
-            //create chrome instance
             driver = new ChromeDriver();
         }
-        //Check if parameter passed as 'Edge'
         else if (browser.equalsIgnoreCase("Edge")) {
-            //set path to Edge.exe
             System.setProperty("webdriver.edge.driver", "C:\\SeleniumDrivers\\ms edge driver\\msedgedriver.exe");
-            //create Edge instance
             driver = new EdgeDriver();
         } else {
-            //If no browser passed throw exception
             throw new Exception("Browser is not correct");
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
